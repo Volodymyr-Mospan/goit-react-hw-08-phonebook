16bf55823981892327aa0be7a20cda04607c7720
Volodymyr <mospan.volodymyr@gmail.com>
2023-03-26 12:44:14 +0300

    adds some ctyles

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from 'react-router-dom';
+import { useAuth } from 'hooks/useAuth';
+import { NavigationLink, NavigationStyled } from './Navigation.styled';
+
+export const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <NavigationStyled>
+      <NavigationLink to="/">Home</NavigationLink>
+      {isLoggedIn && <NavigationLink to="/contacts">Contacts</NavigationLink>}
+    </NavigationStyled>
+  );
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,37 @@
+import { NavLink } from 'react-router-dom';
+
 export const Home = () => {
+  const styles = {
+    container: {
+      paddingTop: '100px',
+      minHeight: 'calc(100vh - 50px)',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      // justifyContent: 'center',
+    },
+    link: {
+      textDecoration: 'none',
+      color: 'black',
+    },
+  };
   return (
-    <>
+    <div style={styles.container}>
       <h2>Welcome to the Contact Book</h2>
-      <p>Please, LogIn or Register to view your contacts</p>
-    </>
+      <p>
+        Please,{' '}
+        <NavLink style={styles.link} to="/login">
+          Log In
+        </NavLink>{' '}
+        or{' '}
+        <NavLink style={styles.link} to="/register">
+          Register
+        </NavLink>{' '}
+        to view your{' '}
+        <NavLink style={styles.link} to="/contacts">
+          Contacts
+        </NavLink>
+      </p>
+    </div>
   );
 };
