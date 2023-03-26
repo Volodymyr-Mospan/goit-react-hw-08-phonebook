e4203d2963bcf2d6dc9f6c811d06edc3ddeed06c
Volodymyr <mospan.volodymyr@gmail.com>
2023-03-26 13:50:41 +0300

    adds some styles

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,20 +1,19 @@
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
 import { useAuth } from 'hooks/useAuth';
-import { UserMenuButtonStyled } from './UserMenu.styled';
+import { UserMenuButtonStyled, UserMenuStyled } from './UserMenu.styled';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
 
   return (
-    <div>
-      <p>
-        Welcome, <b>{user.name}</b>
-      </p>
+    <UserMenuStyled>
+      <b>{user.email}</b>
+
       <UserMenuButtonStyled type="button" onClick={() => dispatch(logOut())}>
         Logout
       </UserMenuButtonStyled>
-    </div>
+    </UserMenuStyled>
   );
 };
--- src/components/UserMenu/UserMenu.styled.js
+++ src/components/UserMenu/UserMenu.styled.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
+export const UserMenuStyled = styled.div`
+  width: min-content;
+
+  background-color: rgba(255, 255, 255, 0.2);
+  padding: 10px 25px;
+  border: none;
+  border-bottom: 1px solid black;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
 export const UserMenuButtonStyled = styled.button`
   width: 100%;
   margin-top: 20px;
